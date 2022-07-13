b66f96220250b58d66df9327715800f46bfecbfe
Chung Wu <chungwu@plasmic.app>
2022-07-12 18:58:59 -0700

    Add opt-style-font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,10 @@ export default function MyDocument() {
   return (
     <Html>
       <Head>
-        <link
+        {/* <link
           href="https://fonts.googleapis.com/css2?family=Inter%3Aital%2Cwght%400%2C400%3B0%2C600%3B0%2C700%3B0%2C800%3B0%2C900&family=Inconsolata%3Aital%2Cwght%400%2C400%3B0%2C600%3B0%2C700%3B0%2C800%3B0%2C900&family=Lato%3Aital%2Cwght%400%2C400%3B0%2C700%3B0%2C900&family=Roboto%3Aital%2Cwght%400%2C400%3B0%2C500%3B0%2C700%3B0%2C900&display=swap"
           rel="stylesheet"
-        />
+        /> */}
       </Head>
       <body>
         <Main />
